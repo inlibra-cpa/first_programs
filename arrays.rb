c0c416999d1ff15523a9b6704e9b91d54abca2aa
inlibra@gmail.com <inlibra@gmail.com>
2019-10-29 19:43:21 +0000

    empty full_name file

---
+++ arrays.rb
@@ -0,0 +1,22 @@
+a = [1, 5, 6] #array is defined
+b = a [1+1] + 1 #simplify this expression
+puts b 
+puts a.length
+puts a.count
+a << 7
+puts a.inspect
+puts a.length
+puts a[3]
+a = [1, 2, 3, 4]
+a.include? (1) # evaluates to true
+a.include? (7) # evaluates to false
+a = [[1, 2, 3], [4, 5, 6]]
+a.length #evaluates to 2
+a [0] #evaluates to [1, 2, 3]
+a [1] #evaluates to [4, 5, 6]
+a [0][2] #evaluates to 3, because a[0] evaluates to [1,2,3] and the value at index 2 in that array is 3
+a = ["a", "b", "c"]
+x = a.join (".")
+
+
+
